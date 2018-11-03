a1dd3155f6f0b833203423eebfb3d09dc7977ba2
Dan Miller <drmiller1@madisoncollege.edu>
2018-11-02 20:48:49 -0500

    Commit SoapActivity3 code.

---
+++ src/test/java/gov/noaa/NdfdXMLBindingStubTest.java
@@ -0,0 +1,28 @@
+package gov.noaa;
+
+import org.junit.jupiter.api.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NdfdXMLBindingStubTest {
+
+    @Test
+    void latLonListZipCode() throws Exception {
+        NdfdXMLBindingStub binding = (NdfdXMLBindingStub) new NdfdXMLLocator().getndfdXMLPort();
+        String result = binding.latLonListZipCode("53711");
+
+        JAXBContext jaxbContext = JAXBContext.newInstance(DwmlType.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+        // specify the location and name of xml file to be read
+        File XMLfile = new File("src/main/resources/response.xsd");
+        DwmlType dwml = (DwmlType) jaxbUnmarshaller.unmarshal(XMLfile);
+        assertEquals(dwml.getLatLonList(), result);
+        //assertEquals("dwml", result);
+    }
+}
